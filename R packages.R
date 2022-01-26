e884182d6ae97767be9b46db97bdf8dc2611a98d
stynan2 <98417441+stynan2@users.noreply.github.com>
2022-01-26 14:54:17 +0000

    Add files via upload

---
+++ R packages.R
@@ -0,0 +1,9 @@
+install.packages(c("rmarkdown", "evaluate", "asserthat", "ggplot2", "purrr", "tibble", "dplyr", "tidyr", "stringr",
+                   "readr", "forcats", "tidyverse"), 
+                 dependencies = TRUE,
+                 repos = 'http://cran.us.r-project.org')
+
+install.packages(c("rmarkdown", "car", "aplore3", "titanic", "evaluate", "asserthat", "ggplot2", "purrr", "tibble", "dplyr", "tidyr", "stringr",
+                   "readr", "forcats", "tidyverse"), 
+                 dependencies = TRUE,
+                 repos = 'http://cran.us.r-project.org')
