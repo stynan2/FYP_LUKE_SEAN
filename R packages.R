0721b9c2fb965c2ece08807613bfd978a043eeb0
stynan2 <98417441+stynan2@users.noreply.github.com>
2022-02-27 13:40:05 +0000

    Delete R packages.R

--- R packages.R
+++
@@ -1,5 +0,0 @@
-install.packages(c("stargazer", "ggplot2","dplyr", 
-                   "readr", "tidyverse"), 
-                 dependencies = TRUE,
-                 repos = 'http://cran.us.r-project.org')
-
