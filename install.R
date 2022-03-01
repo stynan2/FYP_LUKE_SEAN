912984b16857b462e533d6ec1fe2d9022c222ea9
stynan2 <98417441+stynan2@users.noreply.github.com>
2022-03-01 18:08:20 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages(c("stargazer", "ggplot2","dplyr", 
-                   "readr", "tidyverse"), 
+                   "readr", "tidyverse","lmtest"), 
                  dependencies = TRUE,
                  repos = 'http://cran.us.r-project.org')
